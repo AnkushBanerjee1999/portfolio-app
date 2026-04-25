accf10ea9af5cf19dc10167f776a83a6bb478da0
AnkushBanerjee1999 <ankushbanerjee900@gmail.com>
2026-04-25 12:11:04 +0530

    feat: implement shopping cart page with cart management and navigation components

--- src/pages/CheckoutConfirmationPage.js
+++ src/pages/CheckoutConfirmationPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../ThemeContext';
-import { useSearch } from '../SearchContext';
 import { useCart } from '../CartContext';
 
 const CheckoutConfirmationPage = () => {
--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useTheme } from '../ThemeContext';
-import { useSearch } from '../SearchContext';
 import { useCart } from '../CartContext';
 
 const ProductDetailPage = () => {
